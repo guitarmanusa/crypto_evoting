41e26480f5e6a5db7fc8b95df9facde539c71d06
Kyle Francis <guitarman_usa@yahoo.com>
2016-11-16 15:04:27 -0500

    Updated voter interface to use Gtk.Assistant

--- test_admin.py
+++ test_admin.py
@@ -40,7 +40,45 @@ if is_admin():
     login = builder.get_object("button_login")
     login.connect("clicked", login_clicked)
 else:
+    def apply_button_clicked(assistant):
+        print("The 'Apply' button has been clicked")
+
+    def close_button_clicked(assistant):
+        print("The 'Close' button has been clicked")
+        Gtk.main_quit()
+
+    def cancel_button_clicked(assistant):
+        print("The 'Cancel' button has been clicked")
+        Gtk.main_quit()
+
+    def checkbutton_toggled(checkbutton):
+        assistant.set_page_complete(box1, checkbutton.get_active())
+
     builder.add_from_file("voter_interface.glade")
+
+    assistant = builder.get_object("main_window")
+    print assistant
+    assistant.connect("cancel", cancel_button_clicked)
+    assistant.connect("close", close_button_clicked)
+    assistant.connect("apply", apply_button_clicked)
+
+    page1 = builder.get_object("label1")
+    assistant.set_page_complete(page1, True)
+
+    page2 = builder.get_object("label2")
+    assistant.set_page_complete(page2, True)
+
+    page3 = builder.get_object("label3")
+    assistant.set_page_complete(page3, True)
+
+    page4 = builder.get_object("box4")
+    assistant.set_page_complete(page4, True)
+
+    page5 = builder.get_object("box5")
+    assistant.set_page_complete(page5, True)
+
+    page6 = builder.get_object("box6")
+    assistant.set_page_complete(page6, True)
 
 window = builder.get_object("main_window")
 window.connect("delete-event", Gtk.main_quit)
