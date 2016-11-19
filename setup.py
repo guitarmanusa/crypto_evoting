7e6edf4b85bf8a8287135bf391829a80d9cb38ca
Kyle Francis <guitarman_usa@yahoo.com>
2016-11-19 16:24:36 -0500

    Successful Windows build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 ## Get the site-package folder, not everybody will install
 ## Python into C:\PythonXX
 site_dir = site.getsitepackages()[1]
-include_dll_path = os.path.join(site_dir, "gtk")
+include_dll_path = os.path.join(site_dir, "gnome")
 
 ## Collect the list of missing dll when cx_freeze builds the app
 missing_dll = ['libgtk-3-0.dll',
@@ -17,14 +17,14 @@ missing_dll = ['libgtk-3-0.dll',
                'libpangocairo-1.0-0.dll',
                'libpangoft2-1.0-0.dll',
                'libpangowin32-1.0-0.dll',
-               'libgnutls-26.dll',
+               'libgnutls-28.dll',
                'libgcrypt-11.dll',
                'libp11-kit-0.dll'
 ]
 
 ## We also need to add the glade folder, cx_freeze will walk
 ## into it and copy all the necessary files
-glade_folder = 'glade'
+glade_folder = 'lib/glade'
 
 ## We need to add all the libraries too (for themes, etc..)
 gtk_libs = ['etc', 'lib', 'share']
@@ -35,7 +35,9 @@ for dll in missing_dll:
     include_files.append((os.path.join(include_dll_path, dll), dll))
 
 ## Let's add glade folder and files
-include_files.append((glade_folder, glade_folder))
+include_files.append((os.path.join(include_dll_path, glade_folder), glade_folder))
+include_files.append("voter_interface.glade")
+include_files.append("admin_login.glade")
 
 ## Let's add gtk libraries folders and files
 for lib in gtk_libs:
@@ -54,17 +56,17 @@ executables = [
 ]
 
 buildOptions = dict(
-    compressed = False,
+    #compressed = False,
     includes = ["gi"],
     packages = ["gi"],
     include_files = include_files
     )
 
 setup(
-    name = "test_gtk3_app",
-    author = "Gian Mario Tagliaretti",
+    name = "E-Voting CSCI 4320/6320",
+    author = "Kyle Francis, Ben Espey, Damon Gass",
     version = "1.0",
-    description = "GTK 3 test",
+    description = "An example E-Voting program utilizing homomorphic encryption.",
     options = dict(build_exe = buildOptions),
     executables = executables
 )
